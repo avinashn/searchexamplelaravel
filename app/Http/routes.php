539ae8a3ff342112655f5f134104a5ebdf4e72ab
nashy <nethalaavinash94@gmail.com>
2016-04-30 12:29:15 +0530

    Demo of Search functionality in Laravel

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,15 @@
 <?php
+use App\User;
+use Illuminate\Support\Facades\Input;
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get ( '/', function () {
+	return view ( 'welcome' );
+} );
+Route::post ( '/search', function () {
+	$q = Input::get ( 'q' );
+	$user = User::where ( 'name', 'LIKE', '%' . $q . '%' )->orWhere ( 'email', 'LIKE', '%' . $q . '%' )->get ();
+	if (count ( $user ) > 0)
+		return view ( 'welcome' )->withDetails ( $user )->withQuery ( $q );
+	else
+		return view ( 'welcome' )->withMessage ( 'No Details found. Try to search again !' );
+} );
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,49 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Laravel</title>
+<head>
+<title>Search functionality - justLaravel.com</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet"
+	href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+</head>
+<body>
+	<div class="container">
+		<form action="/search" method="POST" role="search">
+			{{ csrf_field() }}
+			<div class="input-group">
+				<input type="text" class="form-control" name="q"
+					placeholder="Search users"> <span class="input-group-btn">
+					<button type="submit" class="btn btn-default">
+						<span class="glyphicon glyphicon-search"></span>
+					</button>
+				</span>
+			</div>
+		</form>
+		<div class="container">
+			@if(isset($details))
+			<p> The Search results for your query <b> {{ $query }} </b> are :</p>
+			<h2>Sample User details</h2>
+			<table class="table table-striped">
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Email</th>
+					</tr>
+				</thead>
+				<tbody>
+					@foreach($details as $user)
+					<tr>
+						<td>{{$user->name}}</td>
+						<td>{{$user->email}}</td>
+					</tr>
+					@endforeach
+				</tbody>
+			</table>
+			@elseif(isset($message))
+			<p>{{ $message }}</p>
+			@endif
+		</div>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-            </div>
-        </div>
-    </body>
+</body>
 </html>
