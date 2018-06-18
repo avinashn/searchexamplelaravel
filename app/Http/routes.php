3eb0dc32c6c25441627678aaae5cf42f8ad621bc
Avinash Nethala <nethalaavinash94@gmail.com>
2018-06-18 11:36:32 +0530

    Updated empty query condition

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -7,9 +7,12 @@ Route::get ( '/', function () {
 } );
 Route::post ( '/search', function () {
 	$q = Input::get ( 'q' );
-	$user = User::where ( 'name', 'LIKE', '%' . $q . '%' )->orWhere ( 'email', 'LIKE', '%' . $q . '%' )->get ();
-	if (count ( $user ) > 0)
-		return view ( 'welcome' )->withDetails ( $user )->withQuery ( $q );
-	else
-		return view ( 'welcome' )->withMessage ( 'No Details found. Try to search again !' );
+	if($q != ""){
+		$user = User::where ( 'name', 'LIKE', '%' . $q . '%' )->orWhere ( 'email', 'LIKE', '%' . $q . '%' )->get ();
+		if (count ( $user ) > 0)
+			return view ( 'welcome' )->withDetails ( $user )->withQuery ( $q );
+		else
+			return view ( 'welcome' )->withMessage ( 'No Details found. Try to search again !' );
+	}
+	return view ( 'search-functionality-in-laravel/welcome' )->withMessage ( 'No Details found. Try to search again !' );
 } );
